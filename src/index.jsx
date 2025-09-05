ef99d172026c43b94c4ab89ce24c15386f0bb951
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-05 17:08:48 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
-// src/main.jsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./Routes.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import ErrorBoundary from './components/ErrorBoundary.jsx';
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
