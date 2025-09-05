6ca83266633a9aa5a9e7aeda5267ae2ca52a55c8
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-05 16:38:00 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
+// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/tailwind.css";
-import "./styles/index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./Routes.jsx";
+import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(<App />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </React.StrictMode>
+);
