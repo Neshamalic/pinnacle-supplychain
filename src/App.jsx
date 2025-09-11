56ba6a7658de1ec46093672f5e8e2d312de1940e
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-10 23:19:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./Routes.jsx";
+import AppRoutes from "./Routes";
 
 export default function App() {
   return (
