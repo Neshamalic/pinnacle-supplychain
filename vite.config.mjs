131de50d43919faa4c6e5341d86de019f0b90f7c
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-12 13:00:59 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,23 @@
 // vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
 import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { "@": path.resolve(__dirname, "src") }
-  },
   build: {
-    outDir: "dist",                 // <-- que coincida con vercel.json
-    chunkSizeWarningLimit: 2000
-  }
+    outDir: "dist",              // <- importante: dist
+    chunkSizeWarningLimit: 2000,
+  },
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "src") },
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: [".amazonaws.com", ".builtwithrocket.new"],
+  },
 });
