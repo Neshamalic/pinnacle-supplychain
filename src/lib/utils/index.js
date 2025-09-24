49a7b22528ee3578ef724895346442c4e096c57d
Neshamalic <212040406+Neshamalic@users.noreply.github.com>
2025-09-24 11:15:48 -0300

    Update index.js

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,2 +1,3 @@
-// Permite importar con: import {...} from "@/lib/utils"
-export * from "../utils.js";
+// re-exporta todo desde utils.js para permitir import "@/lib/utils"
+export * from '../utils.js';
+export { default } from '../utils.js'; // por si alguien hace import default
